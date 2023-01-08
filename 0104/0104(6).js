ed1f2ae86182893bb1efa759e5db89d8bc54e0aa
hayuk1020 <122197362+hayuk1020@users.noreply.github.com>
2023-01-09 00:34:17 +0900

    Add files via upload

---
+++ 0104/0104(6).js
@@ -0,0 +1,7 @@
+var arr=[1,2,3];
+
+arr.unshift("'begin'");
+arr.push("'end'");
+
+
+console.log(arr);
---
+++ 0104/0104.js
@@ -0,0 +1,7 @@
+var prefix = "program";
+var suffix = "mer";
+var word = prefix.concat(suffix);
+
+var word_length = word.length;
+
+console.log("word : ", word);
